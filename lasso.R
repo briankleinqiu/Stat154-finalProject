9201242fef4d27d792698dacd8994e4fb2cdd612
Brian Qiu <brianqiu2000@gmail.com>
2016-04-23 16:54:13 -0700

    added xgboost

--- lasso.R
+++ lasso.R
@@ -22,12 +22,15 @@ y_train = y[train_indices]
 y_holdout = y[holdout_indices]
 ##################
 
-#lasso with full dataset
-lasso = cv.glmnet(X, y, nfolds = 10)
+#TRAIN LASSO
+y_train = as.integer(y_train)
+y_holdout = as.integer(y_holdout)
+lasso = cv.glmnet(X_train, y_train, nfolds = 10)
 
-lasso$lambda.min #best lambda is .001150176
-1 - min(lasso$cvm) #best prediction is 82.3235%
+lasso$lambda.min #best lambda is .001135525
+1 - min(lasso$cvm) #best cross-validated prediction is 82.3235%
 
+#PREDICT ON XTEST
 yhat = predict(lasso, newx = Xtest, s = "lambda.min")
 yhat[yhat >= .5] = 1
 yhat[yhat < .5] = 0
@@ -36,22 +39,29 @@ submission = data.frame(id = 1:length(yhat), y = yhat)
 colnames(submission) = c("id", "y")
 write.csv(submission, file = "lassoSubmission.csv", row.names = FALSE) #kaggle score: 75.9%
 
-#lasso's prediction on holdout is .77, much closer to Kaggle score
+#PREDICT ON HOLDOUT
 yhat = predict(lasso, newx = X_holdout, s = "lambda.min")
 yhat[yhat >= .5] = 1
 yhat[yhat < .5] = 0
-sum(yhat == y_holdout)/length(yhat)
+sum(yhat == y_holdout)/length(yhat) #77.4%
 
 #sparse logistic
-y_train = as.factor(y_train)
-y_holdout = as.factor(y_holdout)
-sparse_log = cv.glmnet(X_train, y_train, nfolds = 10, family = "binomial", type.measure = "class")
-yhat = predict(sparse_log, newx = X_holdout, s = "lambda.min", type = "class")
-sum(yhat == y_holdout)/length(yhat)
+sparse_log = cv.glmnet(X_train, as.factor(y_train), nfolds = 10, family = "binomial", type.measure = "class")
+yhat_log = predict(sparse_log, newx = X_holdout, s = "lambda.min", type = "class")
+sum(yhat_log == as.factor(y_holdout))/length(yhat) #77.16%
 
 #find sparse indices
 sparse_indices = which(coef(sparse_log, s = "lambda.1se") != 0)
 sparse_indices = as.character(sparse_indices)
 write(sparse_indices, "sparse_indices.txt", sep="\n")
+
+
+library(e1071)
+bayes = naiveBayes(x = X_train, y = as.factor(y_train))
+yhat_bayes =  predict(bayes, newdata = X_holdout) 
+sum(yhat_bayes == as.factor(y_holdout))/length(temp2) #68.4% on Naive Bayes
+
+save(sparse_log, file = "sparse_logistic.rda")
+
 
 
